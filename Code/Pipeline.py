9c4eb9e234fa83d5031ea2e8e3099686a4fc27f8
JVCEmmel <julius.emmel@pm.me>
2020-11-14 14:36:26 +0100

    Functional Code - Showing Images

--- Code/Pipeline.py
+++ Code/Pipeline.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 
-img = cv.imread("~/PowerFolders/Masterarbeit/Bilder/acht/acht1_001.jpg")
+img = cv.imread("/home/julius/PowerFolders/Masterarbeit/Bilder/acht/acht1_001.jpg")
+
+
 
 while cv.waitKey(1) != ord('x'):
     cv.imshow("Bild", img)
