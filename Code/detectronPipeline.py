7b69ba09561ed7bb38c9ef46a0fdece14d8e3ea9
JVCEmmel <julius.emmel@pm.me>
2020-11-23 14:45:59 +0100

    changed display size

--- Code/detectronPipeline.py
+++ Code/detectronPipeline.py
@@ -18,30 +18,31 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
-path = "/home/julius/PowerFolders/Masterarbeit/Bilder/1_Datensaetze/first_annotation_dataset/"
+try:
 
-image = cv2.imread(path + "acht1_036.jpg", 1)
+    path = "/home/julius/PowerFolders/Masterarbeit/Bilder/1_Datensaetze/first_annotation_dataset/"
 
-cv2.imshow("image", image)
-cv2.waitKey(3000)
-x
+    image = cv2.imread(path + "acht1_036.jpg", 1)
+    image = cv2.resize(image, (928,1392))
 
 
-cfg = get_cfg()
+    cfg = get_cfg()
 
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-predictor = DefaultPredictor(cfg)
-outputs = predictor(image)
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(image)
 
-print(outputs["instances"].pred_classes)
-print(outputs["instances"].pred_boxes)
+    print(outputs["instances"].pred_classes)
+    print(outputs["instances"].pred_boxes)
 
-v = Visualizer(image[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-cv2.imshow("image", out.get_image()[:, :, ::-1])
-k = cv2.waitKey(0) & 0xFF
-if k == 32:
+    v = Visualizer(image[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+    while cv2.waitKey(0) != ord('x'):
+        cv2.imshow("image", out.get_image()[:, :, ::-1])
+
+finally:
     cv2.destroyAllWindows()
