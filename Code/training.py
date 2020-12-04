fac0ecc6d35988a7e9c6ab7e125596905716a8f1
JVCEmmel <julius.emmel@pm.me>
2020-12-04 15:50:05 +0100

    generalized the information

--- Code/training.py
+++ Code/training.py
@@ -10,23 +10,23 @@ from detectron2.engine import DefaultTrainer, DefaultPredictor
 from detectron2.config import get_cfg
 import os
 
-path = "/home/julius/PowerFolders/Masterarbeit/Bilder/1_Datensaetze/first_annotation_dataset/"
+path = "/home/julius/PowerFolders/Masterarbeit/Bilder/1_Datensaetze/test/"
 
 # load and register the dataset
-load_coco_json(path + "output.json", path[:-1], "first_anno_ds")
-register_coco_instances("first_anno_ds", {}, path + "output.json", path[:-1])
+load_coco_json(path + "output.json", path[:-1], "test_set")
+register_coco_instances("test_set", {}, path + "output.json", path[:-1])
 
 # get the Metadata
-first_anno_ds_metadata = MetadataCatalog.get("first_anno_ds")
-print(first_anno_ds_metadata)
+test_set_metadata = MetadataCatalog.get("test_set")
+print(test_set_metadata)
 
-dataset_dicts = DatasetCatalog.get("first_anno_ds")
+dataset_dicts = DatasetCatalog.get("test_set")
 
 # display an image of the dataset
 """
 for d in random.sample(dataset_dicts, 1):
     img = cv2.imread(d["file_name"])
-    visualizer = Visualizer(img[:,:,::-1], metadata=first_anno_ds_metadata, scale=0.5)
+    visualizer = Visualizer(img[:,:,::-1], metadata=test_set_metadata, scale=0.5)
     vis = visualizer.draw_dataset_dict(d)
     
     cv2.imshow("Bild", vis.get_image()[:,:,::-1])
@@ -40,32 +40,31 @@ config.merge_from_file(
     "/home/julius/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
 )
 
-config.DATASETS.TRAIN = ("first_anno_ds",)
+config.DATASETS.TRAIN = ("test_set",)
 config.DATASETS.TEST = ()
 config.DATALOADER.NUM_WORKERS = 2
 config.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
+config.OUTPUT_DIR = "/home/julius/PowerFolders/Masterarbeit/detectron - training Output/"
 config.SOLVER.IMS_PER_BATCH = 2
 config.SOLVER.BASE_LR = 0.02
 config.SOLVER.MAX_ITER = (300)
 config.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = (128)
 config.MODEL.ROI_HEADS.NUM_CLASSES = 16
 
-"""
 os.makedirs(config.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(config)
 trainer.resume_or_load(resume=False)
 trainer.train()
-"""
 
 config.MODEL.WEIGHTS = os.path.join(config.OUTPUT_DIR, "model_final.pth")
 config.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-config.DATASETS.TEST = ("first_anno_ds",)
+config.DATASETS.TEST = ("test_set",)
 predictor = DefaultPredictor(config)
 
 for d in random.sample(dataset_dicts, 3):
     im = cv2.imread(d["file_name"])
     outputs = predictor(im)
-    v = Visualizer(im[:,:,::-1], metadata=first_anno_ds_metadata, scale=0.8, instance_mode=ColorMode.IMAGE_BW)
+    v = Visualizer(im[:,:,::-1], metadata=test_set_metadata, scale=0.8, instance_mode=ColorMode.IMAGE_BW)
 
     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
     cv2.imshow("Bild", v.get_image()[:,:,::-1])
