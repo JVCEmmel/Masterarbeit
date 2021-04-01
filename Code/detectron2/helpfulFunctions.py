caf09d4eb67f942135abcc35dcf599dd187a4d2b
JVCEmmel <julius.emmel@pm.me>
2021-04-01 19:33:12 +0200

    reduced functionality. Aiming for deletion

--- Code/detectron2/helpfulFunctions.py
+++ Code/detectron2/helpfulFunctions.py
@@ -8,26 +8,6 @@ try:
         cv.imshow("Bild", np.array(img, dtype=np.uint8))
         cv.waitKey(15000)
         cv.destroyWindow("Bild")
-    
-    def saveImage(img, output_path):
-        cv.imwrite(output_path + "prediction.jpg", img)
-    
-    def splitTrainTest(path, split):
-        if os.path.isdir(path + "train_split") == False:
-            os.makedirs(path + "train_split")
-        if os.path.isdir(path + "test_split") == False:
-            os.makedirs(path + "test_split")
-
-        images = sorted([element for element in os.listdir(path) if (element.endswith(".JPG")) or (element.endswith(".jpg"))])
-        jsons = sorted([element for element in os.listdir(path) if element.endswith(".json")])
-
-        for count in range(100):
-            if count % split == 0:
-                shutil.move(path + images[count], path + "test_split/" + images[count])
-                shutil.move(path + jsons[count], path + "test_split/" + jsons[count])
-            else:
-                shutil.move(path + images[count], path + "train_split/" + images[count])
-                shutil.move(path + jsons[count], path + "train_split/" + jsons[count])
 
 finally:
     print("")
