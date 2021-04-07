4198f94328dee52e6e7c3e0290732bbfd78b520d
Julius Emmel <julius.emmel@pm.me>
2021-04-07 23:11:54 +0200

    Kurzes Skript zum Verändern von Dateinamen

--- Code/Abfall/test.py
+++ Code/Abfall/test.py
@@ -1,10 +1,24 @@
-import time
+import os
+from os.path import isdir, isfile
 
-start = time.localtime()
-time.sleep(10)
-print(time.strftime("%H:%M:%S", start))
-finish = time.localtime()
-print(time.strftime("%H:%M:%S", finish))
-# print(time.strftime("%H:%M:%S", finish) - time.strftime("%H:%M:%S", start))
-duration = finish - start
-print(time.strftime("%H:%M:%S", duration))
+def get_images(path, all_image_names):
+    file_name_list = os.listdir(path)
+
+    for element in file_name_list:
+        if isfile(path+element) & (element.lower().endswith(".jpg")):
+            all_image_names.add((path+element).replace(base_path, ''))
+        elif isdir(path+element):
+            get_images(path + element + "/", all_image_names)
+
+    all_image_names = list(all_image_names)
+    all_image_names.sort()
+    return all_image_names
+
+base_path = "/home/julius/Repos/Masterarbeit/LaTeX/Abbildungen/detections/05,04,2021-15,39,10/"
+
+all_image_names = set()
+all_image_names = get_images(base_path, all_image_names)
+
+for element in all_image_names:
+    new_name = element.replace(" ", "_")
+    os.rename(base_path + element, base_path + new_name)
