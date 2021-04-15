16f4bc6fff3c052228735d7cf19461a9aed06f57
Julius Emmel <julius.emmel@pm.me>
2021-04-15 22:52:04 +0200

    added image dict and json export

--- Code/detectron2/tesseract.py
+++ Code/detectron2/tesseract.py
@@ -1,18 +1,45 @@
+from PIL import Image
+
 import pytesseract, json, os, cv2
 import numpy as np
 
+def pic_split(img):
 
+    #converting into numpy
+    img_array = np.array(img).astype(float)
+
+    #the actual split
+    img_array_blue = img_array[:,:,0]
+    img_array_green = img_array[:,:,1]
+    img_array_red = img_array[:,:,2]
+
+    np.where(img_array_blue<128, 1, 0)
+    np.where(img_array_green<128, 1, 0)
+    np.where(img_array_red<128,1 ,0)
+
+    #converting them back
+    img_blue = Image.fromarray(img_array_blue.astype(np.uint8))
+    img_green = Image.fromarray(img_array_green.astype(np.uint8))
+    img_red = Image.fromarray(img_array_red.astype(np.uint8))
+
+    return img_blue, img_green, img_red
+
+###SET BASIC VIRABLES###
 os.chdir("/home/julius/PowerFolders/Masterarbeit/")
 
 dataset_path = "./1_Datensaetze/first_annotation_dataset/"
 json_path = "./detections/first_annotation_dataset/14,04,2021-11,18/"
 output_path = json_path
 
+
+# gather images
 images = sorted([element for element in os.listdir(dataset_path) if element.lower().endswith(".jpg")])
 
+# read bounding boxes
 with open(json_path + "bounding_boxes.json", "r+") as inputfile:
     bounding_boxes = json.load(inputfile)
 
+# restructure boxes per image
 boxes_per_image = {}
 for count in range(len(images)):
     current_image = bounding_boxes[images[count]]
@@ -24,16 +51,20 @@ for count in range(len(images)):
             else:
                 boxes_per_image[images[count]].append(current_image["prediction_boxes"][count_two])
 
-print(boxes_per_image[images[0]][0])
+    # if theres no text box in image, create an empty list
+    if images[count] not in boxes_per_image:
+        boxes_per_image[images[count]] = []
 
 
+# go over every image
 for element in range(len(images)):
+    # load image
     image = cv2.imread(dataset_path + images[element])
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #first_pixel = image[0, 0]
-    #image = cv2.normalize(image, None, 0, 1, cv2.NORM_MINMAX)
-    text_dump = set()
+    dump_dict = {}
+
+    # go over every box in the image
     for box in range(len(boxes_per_image[images[element]])):
+        #get box cords, length and with and cut text out
         x_one = boxes_per_image[images[element]][box][0]
         x_two = boxes_per_image[images[element]][box][1]
         y_one = boxes_per_image[images[element]][box][2]
@@ -42,33 +73,30 @@ for element in range(len(images)):
         box_width = abs(x_one - y_one)
         box_height = abs(x_two - y_two)
 
-        box_image = image[x_two : x_two + box_height, x_one : x_one + box_width]
+        image_dict = {}
+        image_dict["colored"] = image[x_two : x_two + box_height, x_one : x_one + box_width]
+        image_dict["blue"], image_dict["green"], image_dict["red"] = pic_split(image_dict["colored"])
+        image_dict["gray"] = cv2.cvtColor(image_dict["colored"], cv2.COLOR_BGR2GRAY)
 
-        image_text = "{}".format(pytesseract.image_to_string(box_image, lang="deu"))
-        image_text = image_text.split("\n")
+        for variant in image_dict:
+            text_dump = set()
+            image_text = "{}".format(pytesseract.image_to_string(image_dict[variant], lang="deu"))
+            image_text = image_text.replace("\x0c", "").split("\n")
 
-        if len(image_text) > 0:
-            [text_dump.add(element) for element in image_text]
-        """
-        cv2.imshow("Bild", box_image)
-        cv2.waitKey(20000)
-        cv2.destroyAllWindows()
-        """
-        """
-        cv2.imwrite(output_path + "image_{}_box_{}.jpg".format(element, box), box_image)
-        """
-    with open(output_path + "{}.txt".format(images[element][:-4]), "w+") as output_file:
-        output_file.write(str(text_dump))
+            if len(image_text) > 0:
+                [text_dump.add(text) for text in image_text if len(text) > 0]
+            
+            if variant not in dump_dict:
+                dump_dict[variant] = list(text_dump)
+            else:
+                dump_dict[variant].append(list(text_dump))
 
+    with open(output_path + "{}.json".format(images[element][:-4]), "w+", encoding="utf8") as output_file:
+        json.dump(dump_dict, output_file, indent=4, ensure_ascii=False)
+
+"""
     text_dump = pytesseract.image_to_string(image, lang="deu")
 
     with open(output_path + "{}_complete.txt".format(images[element][:-4]), "w+") as output_file:
         output_file.write(text_dump)
-    """
-    cv2.imwrite(output_path + "test{}.jpg".format(element), image)
-
-    display_image = cv2.resize(image, (image.shape[1] // 3, image.shape[0] // 3))
-    cv2.imshow("Bild", display_image)
-    cv2.waitKey(20000)
-    cv2.destroyAllWindows()
-    """
+"""
